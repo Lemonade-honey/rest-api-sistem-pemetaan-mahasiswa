efaf1bf8c7197da753ab11e6ba6728d0fb5c96d6
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-05-28 21:59:10 +0700

    impl: implementasi KNN pada route test KNN

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union
 from fastapi import FastAPI, HTTPException
+import joblib
 
-from src import CleaningText
+from src import CleaningText, KNNClassification
 from model import File
 from request import ValidateType
 
@@ -34,3 +35,38 @@ async def upload_files(list_file: File)-> dict:
     }
 
     return response
+
+# test model KNN
+@app.post("/test-model/")
+async def test_model_classification(text: str)-> dict:
+
+    if not text:
+        raise HTTPException(status_code=400, detail="text perlu dimasukan")
+    
+    try:
+        # loaded model
+        knn_model = joblib.load('knn_classification_model/knn_classification_model.pkl')
+        vectorizer = joblib.load('knn_classification_model/tfidf_fit_transform.pkl')
+
+        knn = KNNClassification(knn_model, vectorizer)
+
+        predicted_label, probabilitas = knn.predict_label(text)
+
+        response = {
+            'massage' : 'sukses memprediksi label',
+            'data' : {
+                'label-prediksi': knn.label_to_text(predicted_label),
+                'probabilitas': probabilitas
+            }
+        }
+
+        return response
+    
+    except Exception as ex:
+
+        response = {
+            'massage' : 'error server',
+            'error' : ex
+        }
+
+        return response
