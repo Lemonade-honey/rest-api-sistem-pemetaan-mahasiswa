d3e75dba335050f3f1d384c5f41f319f514c4149
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-05-28 12:12:24 +0700

    impl: Model dan Validate pada upload file

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from src import CleaningText
+from model import File
+from request import ValidateType
 
 app = FastAPI()
 
@@ -12,6 +14,23 @@ def index_root()-> dict:
     response = {
         'server' : 'success connect',
         'status' : 200
+    }
+
+    return response
+
+# route upload file
+@app.post("/upload-file/")
+async def upload_files(list_file: File)-> dict:
+
+    if not list_file.files:
+        raise HTTPException(status_code=400, detail="No files provided")
+
+    # validasi
+    ValidateType(['.pdf']).validate_datas(list_file.files)
+    
+    response = {
+        'massage' : 'File berhasil diterima',
+        'data': list_file.files
     }
 
     return response
