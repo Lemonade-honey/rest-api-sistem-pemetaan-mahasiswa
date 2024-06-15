f87af9ca93847c7d4e03a723478e3b5f44937d0f
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-15 18:32:47 +0700

    test: summerize text pada sampel dokumen

--- test/index.py
+++ test/index.py
@@ -1,13 +1,13 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src import ExtractPdf
+from src import ExtractPdf, Summerize
 
 
-file_path = "sample/pdf/docs-1.pdf"
+file_path = "sample/pdf/mpti2.pdf"
 
-extracted_page = ExtractPdf().extract_pdf_to_text(file_path, start_page=0, end_page= 10, tipe="page")
+extracted_page = ExtractPdf().extract_pdf_to_text(file_path, start_page=0, end_page= 50, tipe="page")
 
-find_page = ExtractPdf().find_target_text_page(extracted_page, target_kata=["ringkasan"])
+find_page = ExtractPdf().find_target_text_page(extracted_page, target_kata=["abstrak", "pendahuluan"], hindari_kata=["daftar isi", "table of contents", "contents", "daftar pustaka", "bab ii"])
 
-print(find_page)
+print(Summerize().summarize_text(find_page[0], 4), '\n\n\n', find_page[0])
